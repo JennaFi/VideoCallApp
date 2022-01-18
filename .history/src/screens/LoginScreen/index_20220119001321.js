46db8ab9416dddc04473af6cfee39eb62de06216
JennaFi <fi@mail.com>
2022-01-19 00:17:09 +0300

    Login UI

---
+++ .history/src/screens/LoginScreen/index_20220119001321.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+
+const LoginScreen = () => {
+
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signIn = () => {}
+
+  return (
+    <View style={styles.page}>
+      <TextInput value={userName} onChangeText={setUserName} placeholder='username' style={styles.input} />
+      <TextInput value={password} onChangeText={setPassword} placeholder='password' style={styles.input} />
+
+      <Pressable style={styles.button} onPress={signIn}> 
+        <Text>Sign in</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+    alignItems: 'stretch',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5
+  },
+  button: {
+    backgroundColor: 'dodgerblue',
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center'
+
+  }
+
+})
+
+export default LoginScreen
